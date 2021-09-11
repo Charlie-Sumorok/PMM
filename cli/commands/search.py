f0ff8182598d0c18ffa0b952afeabf6b9b9eeb20
Charlie <77081751+Charlie-Sumorok@users.noreply.github.com>
2021-09-11 16:19:50 -0400

    Update search.py

--- cli/commands/search.py
+++ cli/commands/search.py
@@ -3,8 +3,8 @@
 "search for available packages"
 
 import os
-import requests
 import json
+import requests
 
 data_urls = {
     "brew": {
@@ -37,12 +37,14 @@ data_urls = {
 
 
 def search_npm(name):
+    "search npm for package"
     results_info = os.popen(f"npm search --json {name}").read()
     results = [*map(lambda result: json.loads(result)["name"], results_info)]
     return results
 
 
 def get_npm_info(package, registry_url="auto"):
+    "get info of npm package"
     if registry_url == "auto":
         registry_url = "https://registry.npmjs.com"
     response = requests.get(f"{registry_url}/{package}")
