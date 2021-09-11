3d749ccce3ed7e1c0773defc2da0f9d6b357c11e
Charlie <77081751+Charlie-Sumorok@users.noreply.github.com>
2021-09-11 16:15:01 -0400

    Update search.py

--- cli/commands/search.py
+++ cli/commands/search.py
@@ -1,54 +1,49 @@
 #! /usr/bin/env python
 
-'search for available packages'
+"search for available packages"
 
 import os
 import requests
+import json
 
 data_urls = {
-	'brew': {
-		'macOS': {
-			'formula': 'https://formulae.brew.sh/api/formula/{name}.json',
-			'cask': 'https://formulae.brew.sh/api/cask/{name}.json',
-			'analytics': {
-				'one-category': 'https://formulae.brew.sh/api/analytics/{category}/{days}.json',
-				'all': {
-					'all': 'https://formulae.brew.sh/api/analytics/{category}/{repo}/{days}.json',
-					'core': 'https://formulae.brew.sh/api/analytics/{category}/homebrew-core/{days}.json',
-					'cask': 'https://formulae.brew.sh/api/analytics/{category}/homebrew-cask/{days}.json',
-				}
-			}
-		},
-		'Linux': {
-			'formula': 'https://formulae.brew.sh/api/formula-linux/{name}.json',
-			'cask': 'https://formulae.brew.sh/api/cask/{name}.json',
-			'analytics': {
-				'one-category': 'https://formulae.brew.sh/api/analytics-linux/{category}/{days}.json',
-				'all': {
-					'all': 'https://formulae.brew.sh/api/analytics/{category}/{repo}/{days}.json',
-					'core': 'https://formulae.brew.sh/api/analytics/{category}/linuxbrew-core/{days}.json',
-					'cask': 'https://formulae.brew.sh/api/analytics/{category}/homebrew-cask/{days}.json',
-				}
-			}
-		}
-	}
+    "brew": {
+        "macOS": {
+            "formula": "https://formulae.brew.sh/api/formula/{name}.json",
+            "cask": "https://formulae.brew.sh/api/cask/{name}.json",
+            "analytics": {
+                "one-category": "https://formulae.brew.sh/api/analytics/{category}/{days}.json",
+                "all": {
+                    "all": "https://formulae.brew.sh/api/analytics/{category}/{repo}/{days}.json",
+                    "core": "https://formulae.brew.sh/api/analytics/{category}/homebrew-core/{days}.json",
+                    "cask": "https://formulae.brew.sh/api/analytics/{category}/homebrew-cask/{days}.json",
+                },
+            },
+        },
+        "Linux": {
+            "formula": "https://formulae.brew.sh/api/formula-linux/{name}.json",
+            "cask": "https://formulae.brew.sh/api/cask/{name}.json",
+            "analytics": {
+                "one-category": "https://formulae.brew.sh/api/analytics-linux/{category}/{days}.json",
+                "all": {
+                    "all": "https://formulae.brew.sh/api/analytics/{category}/{repo}/{days}.json",
+                    "core": "https://formulae.brew.sh/api/analytics/{category}/linuxbrew-core/{days}.json",
+                    "cask": "https://formulae.brew.sh/api/analytics/{category}/homebrew-cask/{days}.json",
+                },
+            },
+        },
+    }
 }
 
+
 def search_npm(name):
-	results_info = os.popen(f"npm search --json {name}").read()
-	results = [*map(
-		lambda result: result['name'],
-		results_info
-	)
-	return results
+    results_info = os.popen(f"npm search --json {name}").read()
+    results = [*map(lambda result: json.loads(result)["name"], results_info)]
+    return results
 
 
-def get_npm_info(
-	package,
-	registry_url = 'auto'
-):
-	if registry_url == 'auto':
-		registry_url = 'https://registry.npmjs.com'
-	response = requests.get(f'{registry_url}/{package}')
-	return response.json()
-
+def get_npm_info(package, registry_url="auto"):
+    if registry_url == "auto":
+        registry_url = "https://registry.npmjs.com"
+    response = requests.get(f"{registry_url}/{package}")
+    return response.json()
