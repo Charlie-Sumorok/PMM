755fc6501891afd8805dcc935f3cd70803e5e009
Charlie <77081751+Charlie-Sumorok@users.noreply.github.com>
2021-09-12 16:38:58 -0400

    Update urls.ts

--- app/src/menu_bar/helper_functions/github/urls.ts
+++ app/src/menu_bar/helper_functions/github/urls.ts
@@ -3,5 +3,6 @@ export const form_query = (query_map: Map<string, string>) => {
 	for (const key of query_map.keys()) {
 		query.push(`${key}=${query_map.get(key)}`);
 	}
+
 	return query.join('&');
 };
